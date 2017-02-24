f6ff68cb059b1be1fa5c4c41c47c410ec5880d8b
David McLaughlin & Jordan Munoz & Tiffany Ann Huey <pair+davidmclaughlin208.jtmunoz.tiffanyannhuey@devbootcamp.com>
2017-02-23 21:46:56 -0800

    finally got the new answer form/route to work

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,25 +1,17 @@
-# new answer
-get '/questions/:id/answers/new' do
-  if currently_logged_in?
-  question = current_question
-  erb :"/answers/new"
-  else
-    @login_error = "You need to be logged in to answer a question"
-    erb :"/questions/#{question.id}" #this is broken
-  end
-end
 
 # create a new answer to the question
 post '/questions/:id/answers/new' do
- p current_user.to_s
- p current_question.to_s
-  # answer = Answer.new( description: params[:description], author_id: current_user.id , question_id: current_question.id )
-  # if answer
-  #   answer.save
-  #   erb :"/questions/#{current_question.id}"
-  # else
-  #   @answer_error = "There was an issue submitting your answer. Try again."
-  #   erb :"/questions/#{current_question.id}/answers/new"
-  # end
+
+  user = User.find_by(id: session[:user_id])
+  question = Question.find(params[:id])
+  answer = Answer.new( description: params[:description], question_id: question.id  )
+  if currently_logged_in? && answer
+    answer.author_id = user.id
+    answer.save
+    redirect "/questions/#{question.id}"
+  else
+    @answer_error = "There was an issue submitting your answer. Try again."
+    erb :"/questions/show"
+  end
 end
 
