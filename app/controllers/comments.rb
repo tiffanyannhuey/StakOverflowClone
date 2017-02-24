92839f60252856a1d13723dccb495edfed7b0af6
Jordan Munoz <jordantmunoz@gmail.com>
2017-02-24 15:46:59 -0800

    updates controller and view for comments

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,14 +1,9 @@
-
-
 post '/questions/:id/answers/:answer_id/comments' do
-	
 	comment = Comment.new(description: params[:comment],
 												commentable: Answer.find_by(id: params[:answer_id]),
 												author_id: session[:id]
 												)
-	puts comment.inspect
 	if comment.save
-		puts comment.inspect
 		redirect "/questions/#{current_question.id}"
 	else
 		@errors = comment.errors.full_messages
@@ -19,4 +14,17 @@ end
 get '/questions/:id/comments/new' do
 	current_question
 	erb :'comments/new'
+end
+
+post '/questions/:question_id/comments' do
+	comment = Comment.new(description: params[:comment],
+												commentable: Question.find_by(id: params[:question_id]),
+												author_id: session[:id]
+												)
+	if comment.save
+		redirect "/questions/#{comment.commentable.id}"
+	else
+		@errors = comment.errors.full_messages
+		erb :'questions/show'
+	end
 end
