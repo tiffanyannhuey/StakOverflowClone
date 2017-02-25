ee7034a37c007ffc2dd4b228f8fb326b887f81db
David McLaughlin & Jordan Munoz & Tiffany Ann Huey <pair+davidmclaughlin208.jtmunoz.tiffanyannhuey@devbootcamp.com>
2017-02-25 13:30:33 -0800

    Try vote coloring

--- app/helpers/sessions_helpers.rb
+++ app/helpers/sessions_helpers.rb
@@ -1,11 +1,11 @@
 helpers do
 
 	def current_user
-  	@user ||= User.find_by(id: session[:user_id])
+		@curent_user ||= User.find_by(id: session[:user_id])
 	end
 
 	def currently_logged_in?
-  	current_user ? true : false
+		current_user ? true : false
 	end
 
 end
