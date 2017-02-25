b8b6dd9705ac0dbd24adc749b5e558d03ffb756d
David McLaughlin & Jordan Munoz & Tiffany Ann Huey <pair+davidmclaughlin208.jtmunoz.tiffanyannhuey@devbootcamp.com>
2017-02-24 19:24:25 -0800

    Merge

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -2,8 +2,8 @@ post "/voteable/:votable_type/:votable_id/votes/:value" do
   redirect "/questions/#{params[:question_id]}" if !current_user
 
   new_vote = current_user.votes.new(votable_id:    params[:votable_id],
-                                    votable_type: params[:votable_type],
-                                    value: params[:value].to_i)
+                                    votable_type:  params[:votable_type],
+                                    value:         params[:value].to_i)
   if new_vote.valid?
     new_vote.save
     redirect "/questions/#{params[:question_id]}"
