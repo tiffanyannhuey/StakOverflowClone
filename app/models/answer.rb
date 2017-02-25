907b0e2a90e3c538926a4525d1d94e9bea93b071
tiffanyannhuey <87tiffanyhuey@gmail.com>
2017-02-24 19:14:20 -0800

    add user profile page

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+  belongs_to :question
   belongs_to :author, class_name: :User, foreign_key: :author_id
 
   scope :comments, -> {all.order(:created_at)}
