245fc7ef254c5e88289b8f1efb3bce658dd2813f
David McLaughlin & Jordan Munoz & Tiffany Ann Huey <pair+davidmclaughlin208.jtmunoz.tiffanyannhuey@devbootcamp.com>
2017-02-25 15:45:37 -0800

    Vote coloring with ajax mostly working

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,17 +1,26 @@
 post "/votable/:votable_type/:votable_id/votes/:value" do
-  redirect "/questions/#{params[:question_id]}" if !current_user
-  puts params
+  redirect "/questions/#{params[:question_id]}" if !current_user || !["-1","1"].include?(params[:value])
+
   new_vote = current_user.votes.new(votable_id:    params[:votable_id],
                                     votable_type:  params[:votable_type],
                                     value:         params[:value].to_i)
-  puts new_vote
   if new_vote.valid?
     new_vote.save
-    redirect "/questions/#{params[:question_id]}"
+    if request.xhr?
+      new_vote.value == 1 ? "upvoted" : "downvoted"
+    else
+      redirect "/questions/#{params[:question_id]}"
+    end
   else
     old_vote = Vote.find_by(user: current_user, votable_id: params[:votable_id], votable_type: params[:votable_type])
     old_vote.destroy
     new_vote.save
-    redirect "/questions/#{params[:question_id]}"
+    p request
+    if request.xhr?
+      new_vote.value == 1 ? "upvoted" : "downvoted"
+    else
+      puts "tryna redirect"
+      redirect "/questions/#{params[:question_id]}"
+    end
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -14,4 +14,38 @@ $(document).ready(function() {
 				$('nav').html(response)
 			});
 	})
+
+
+	var voting = function(event){
+		event.preventDefault();
+		// Check if there is a user logged in
+		if ($('#profile').length > 0){
+			var arrow = $(this);
+			var route = arrow.closest('form').attr('action');
+			$.ajax({
+				url: route,
+				method: "post"
+			})
+			.done(function(response){
+				// console.log(arrow.closest("form"));
+				// console.log(arrow.closest("form").siblings("form"));
+				// console.log(arrow.closest("form").siblings("form").find(".upvoted, .downvoted"));
+				arrow.closest("form").siblings("form").find(".upvoted, .downvoted").css("border-bottom-color", "#a1a1a1").css("border-top-color", "#a1a1a1");
+				// console.log(arrow);
+				// console.log(response);
+				arrow.css("border-bottom-color", "").css("border-top-color", "").removeClass().addClass(response);
+			})
+
+		} else {
+			//Tell them they need to be logged in
+			console.log("Not logged in");
+			$(this).closest("form").append("<div class='vote-not-logged-in'>You need to be logged in to vote</div>")
+		}
+	}
+
+	// Dynamically bind to all possible classes the arrows can have
+	$('body').on('click', '.arrow-up-btn',   voting);
+	$('body').on('click', '.arrow-down-btn', voting);
+	$('body').on('click', '.upvoted',   	 voting);
+	$('body').on('click', '.downvoted',      voting);
 });
