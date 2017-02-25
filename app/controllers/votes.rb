b245c73a4241edcadf80a1e6059bbf4e69478e0d
David McLaughlin & Jordan Munoz & Tiffany Ann Huey <pair+davidmclaughlin208.jtmunoz.tiffanyannhuey@devbootcamp.com>
2017-02-24 19:20:04 -0800

    Implement voting

---
+++ app/controllers/votes.rb
@@ -0,0 +1,16 @@
+post "/voteable/:votable_type/:votable_id/votes/:value" do
+  redirect "/questions/#{params[:question_id]}" if !current_user
+
+  new_vote = current_user.votes.new(votable_id:    params[:votable_id],
+                                    votable_type: params[:votable_type],
+                                    value: params[:value].to_i)
+  if new_vote.valid?
+    new_vote.save
+    redirect "/questions/#{params[:question_id]}"
+  else
+    old_vote = Vote.find_by(user: current_user, votable_id: params[:votable_id], votable_type: params[:votable_type])
+    old_vote.destroy
+    new_vote.save
+    redirect "/questions/#{params[:question_id]}"
+  end
+end
