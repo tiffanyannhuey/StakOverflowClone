e21eb01cabf821c0fabb0c761136f323246c02ee
David McLaughlin & Jordan Munoz & Tiffany Ann Huey <pair+davidmclaughlin208.jtmunoz.tiffanyannhuey@devbootcamp.com>
2017-02-24 20:16:31 -0800

    Implement marking best answer

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,6 +1,6 @@
 post "/voteable/:votable_type/:votable_id/votes/:value" do
   redirect "/questions/#{params[:question_id]}" if !current_user
-
+  puts params
   new_vote = current_user.votes.new(votable_id:    params[:votable_id],
                                     votable_type:  params[:votable_type],
                                     value:         params[:value].to_i)
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
 
   validates :votable_id, :votable_type, :value, :user_id, {null:false}
-  validates :user_id, uniqueness: {scope: :votable_type, scope: :votable_id}
+  validates :user_id, uniqueness: {scope: [:votable_type, :votable_id] }
 end
